dd67dd961b0bc37575a0c3ecb956d691954b2cc9
Korneel Eeckhout <korneel@pienter.tech>
2024-03-13 13:17:52 +0100

    update export

--- build/index.js
+++ build/index.js
@@ -1,30 +1,30 @@
 /* Document */
-import Heml from './Heml.js';
-import Head from './Head.js';
-import Body from './Body.js';
+import Heml from './Heml.cjs';
+import Head from './Head.cjs';
+import Body from './Body.cjs';
 
 /* Meta */
-import Meta from './Meta.js';
-import Subject from './Subject.js';
-import Preview from './Preview.js';
+import Meta from './Meta.cjs';
+import Subject from './Subject.cjs';
+import Preview from './Preview.cjs';
 import Base from './Base.js';
-import Font from './Font.js';
-import Style from './Style.js';
+import Font from './Font.cjs';
+import Style from './Style.cjs';
 
 /* Table */
-import { Table, Tr, Td } from './Table.js';
+import { Table, Tr, Td } from './Table.cjs';
 
 /* Grid */
-import Block from './Block.js';
-import Container from './Container.js';
-import Row from './Row.js';
-import Column from './Column.js';
+import Block from './Block.cjs';
+import Container from './Container.cjs';
+import Row from './Row.cjs';
+import Column from './Column.cjs';
 
 /* Content */
-import { H1, H2, H3, H4, H5, H6, P, Ol, Ul, Li, A } from './Typography.js';
-import Hr from './Hr.js';
-import Button from './Button.js';
-import Img from './Img.js';
+import { H1, H2, H3, H4, H5, H6, P, Ol, Ul, Li, A } from './Typography.cjs';
+import Hr from './Hr.cjs';
+import Button from './Button.cjs';
+import Img from './Img.cjs';
 export { /* Document */
 Heml, Head, Body, /* Meta */
 Meta, Subject, Preview, Base, Font, Style, /* Table */
